b23dc8a917af6516193d34fb1a5764a92b03b779
yeghro <yeghro@proton.me>
2024-08-13 15:39:59 -0700

    cleaned up the active rooms button text to stay within box button, added toggle switch in settings to make button bar visible indefinitely with minimal modifications to Room.html, room.js, and localStorage.js

--- public/js/Room.js
+++ public/js/Room.js
@@ -3098,6 +3098,13 @@ function handleSelects() {
         lS.setSettings(localStorageSettings);
         e.target.blur();
     };
+    switchKeepButtonsVisible.onchange = (e) => {
+        isButtonsBarOver = e.currentTarget.checked;
+        localStorageSettings.keep_buttons_visible = isButtonsBarOver;
+        lS.setSettings(localStorageSettings);
+        e.target.blur();
+    };
+
     // audio options
     switchAutoGainControl.onchange = (e) => {
         localStorageSettings.mic_auto_gain_control = e.currentTarget.checked;
@@ -3533,6 +3540,9 @@ function loadSettingsFromLocalStorage() {
     switchPitchBar.checked = isPitchBarEnabled;
     switchSounds.checked = isSoundEnabled;
     switchShare.checked = notify;
+    isButtonsBarOver = localStorageSettings.keep_buttons_visible || false;
+    document.getElementById('switchKeepButtonsVisible').checked = isButtonsBarOver;
+
 
     recPrioritizeH264 = localStorageSettings.rec_prioritize_h264;
     switchH264Recording.checked = recPrioritizeH264;
