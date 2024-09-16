1762e1d127c46a50432350d731a7e1a3c100d71d
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2024-09-16 01:06:51 -0700

    remove imgs on about, reorder page

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -450,8 +450,8 @@ module.exports = {
             We use our Self-hosted Umami to track aggregated usage statistics in order to improve our service.
         */
         enabled: true,
-        src: 'https://stats.mirotalk.com/script.js',
-        id: '41d26670-f275-45bb-af82-3ce91fe57756',
+        src: '', //'https://stats.mirotalk.com/script.js',
+        id: '', //'41d26670-f275-45bb-af82-3ce91fe57756',
     },
     mediasoup: {
         // Worker settings
