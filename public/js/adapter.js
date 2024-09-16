42bd2dad3b6ac08d470dba70734dd774c5ecdc04
TheWildHustle <145122833+TheWildHustle@users.noreply.github.com>
2024-09-15 22:43:51 -0400

    Create file public/js/adapter.js

---
+++ public/js/adapter.js
@@ -0,0 +1,17 @@
+/*!
+ *  Copyright (c) 2014 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree.
+ */
+
+/* More information about adapter.js can be found at:
+ * https://github.com/webrtc/adapter
+ */
+
+(function() {
+  // Adapter.js code goes here.
+  // This is a placeholder for the actual adapter.js code.
+  // You can download the latest version from https://github.com/webrtc/adapter
+})();
